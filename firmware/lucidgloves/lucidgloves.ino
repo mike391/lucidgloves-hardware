c82808a8c87869947e8db592e715d45c50db28c3
Lucas De Bonet <ldebonet@mit.edu>
2021-03-22 07:22:21 -0400

    Allow overdrive >:)

--- firmware/lucidgloves/lucidgloves.ino
+++ firmware/lucidgloves/lucidgloves.ino
@@ -1,7 +1,8 @@
 const bool USING_JOYSTICK = false;  //configure if you're using joystick
 const bool FLIP_X = false;          //flip x axis joystick
 const bool FLIP_Y = false;          //flip x axis joystick
-
+const int calibrationLoops = 20000;
+const int loopTime = 0;
 int sensorValue = 0;
 String commandFromPC = "";
 int loops = 0;
@@ -15,23 +16,17 @@ int yFlip = FLIP_Y?-1:1;
 
 void setup() {
   // put your setup code here, to run once:
-  for (int b = 0; b < 5; b++)
+  /*for (int b = 0; b < 5; b++)
         minFingers[b] = 1023;
-    for (int c = 0; c < 5; c++)
-        maxFingers[c] = 0;
+  for (int c = 0; c < 5; c++)
+        maxFingers[c] = 0;*/
   Serial.begin(115200);
-  //Serial.begin(9600);
-  //Serial.println("Startup Complete" + String(minFingers[4]));
   pinMode(2, INPUT_PULLUP);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  //commandFromPC = Serial.readStringUntil('\n');
-  delay(4);
-  //delay(300);
-  //if (commandFromPC == "READ_VALUES")
-  //if (commandFromPC.length() > 1)
+  delay(loopTime);
   {
       int theseFingers[5] = {1023-analogRead(A4), 1023-analogRead(A3), 1023-analogRead(A2), 1023-analogRead(A1), 1023-analogRead(A0)};
       int joyX = analogRead(A5);
@@ -41,9 +36,10 @@ void loop() {
       if (joyClick == HIGH){
         joyClick = 1;
       }
-      
+
+      //If still in calibration time, run calibration
       for (int i = 0; i < 5; i++){
-        if (loops < 5000){
+        if (loops < calibrationLoops){
           if (theseFingers[i] > maxFingers[i])
             maxFingers[i] = theseFingers[i];
           if (theseFingers[i] < minFingers[i])
@@ -51,13 +47,14 @@ void loop() {
           loops++;
         }
         if (minFingers[i] != maxFingers[i]){
-          calib[i] = (int)((float)(theseFingers[i] - minFingers[i]) / (float)(maxFingers[i] - minFingers[i]) * 1000.0f);
+          calib[i] = (int)((float)(theseFingers[i] - minFingers[i]) / (float)(maxFingers[i] - minFingers[i]) * 1023.0f);
         }
       }
       char stringToPrint[75];
-      //             fingers        joystick  buttons  gestures
+      //                       fingers        joystick  buttons  gestures
       sprintf(stringToPrint, "%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d&%d\n", 
       calib[0], calib[1], calib[2], calib[3], calib[4], USING_JOYSTICK?joyX*xFlip:1023/2, USING_JOYSTICK?joyY*yFlip:1023/2, 1 - joyClick, 0, 0, 0, 0, 0);
       Serial.print(stringToPrint);
+      Serial.flush();
   }
 }
